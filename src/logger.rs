13b327be2607e793c4693f14b66c9ed456c04d61
Martin Levesque <levesque.martin@gmail.com>
2021-04-18 23:23:29 -0400

    wip

---
+++ src/logger.rs
@@ -0,0 +1,34 @@
+use chrono;
+
+// INFO, DEBUG, ERROR
+
+
+const log_level: &str = std::env!("LOG_LEVEL");
+
+
+fn log_level_i(level: &str) -> i32 {
+    match level {
+        "DEBUG" => 0,
+        "INFO" => 1,
+        "ERROR" => 2,
+        _ => -1
+    }
+}
+
+macro_rules! create_function {
+    ($func_name:ident, $current_level:expr) => {
+        pub fn $func_name(msg: &str) {
+            if log_level_i($current_level) >= log_level_i(log_level) {
+                println!("{} ({}) -- {}",
+                    chrono::offset::Utc::now(), $current_level, msg);
+            }
+        }
+    };
+}
+
+
+
+create_function!(info, "INFO");
+create_function!(debug, "DEBUG");
+create_function!(error, "ERROR");
+
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod logger;
+
+fn main() {
+    logger::info("ilo.");
+    println!("Hello, world!");
+}
